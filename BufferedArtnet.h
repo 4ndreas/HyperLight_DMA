f76b7f2dd83cbc0c2f03b749e36c7f35ff829308
Sebastian Ottlik <git@sebastian-ottlik.de>
2021-01-02 19:47:40 +0100

    add support for artsync and seq based frame loss detection

--- BufferedArtnet.h
+++ BufferedArtnet.h
@@ -7,17 +7,21 @@
 
 #define ARTNET_PAKET_MIN_SIZE 20
 
-static const uint8_t artnet_header_fixed[] = { 'A', 'r', 't', '-', 'N', 'e', 't', 0, 0x00, 0x50, 0, 14 };
+static const uint8_t artnet_header_fixed[] = { 'A', 'r', 't', '-', 'N', 'e', 't', 0};
 
 template<unsigned universes>
 class BufferedArtnet
 {
 	uint8_t data[universes][512];
+	uint8_t last_seq[universes];
+	unsigned lost_frames[universes];
 	EthernetUDP udp;
-
+	bool sync_pending;
+	unsigned sync_received_time;
+	unsigned read_max;
 
 public:
-	BufferedArtnet(): data{0} {
+	BufferedArtnet(): data{0}, last_seq{0}, lost_frames{0}, sync_pending(false), sync_received_time(0) {
 	}
 
 	void begin() {
@@ -33,24 +37,54 @@ public:
 		return data[u];
 	}
 
+	bool syncMode() {
+		return (millis() - sync_received_time) < 4000;
+	}
+
+	bool syncPending() {
+		return sync_pending;
+	}
+
+	void clearSyncPending() {
+		sync_pending = false;
+	}
+
 	bool read() {
 		unsigned packet_size = udp.parsePacket();
 
 		if(packet_size == 0)
 			return false;
 
-		if(packet_size < ARTNET_PAKET_MIN_SIZE)
+		uint8_t header[12];
+		if(udp.read(header, 12) != 12)
 			return true;
 
-		uint8_t header[18];
-		udp.read(header, 18);
-
-		for(int i=0; i < 12; ++i)
+		for(int i=0; i < 8; ++i)
 			if(header[i] != artnet_header_fixed[i])
 				return true;
 
-		uint16_t u = header[14] | (uint16_t(header[15]) << 8);
-		uint16_t size = header[17] | (uint16_t(header[16]) << 8);
+		uint16_t op = header[8] | (uint16_t(header[9]) << 8);
+		uint16_t ver = header[11] | (uint16_t(header[10]) << 8);
+
+		if(ver < 14)
+			return true;
+
+		if(op == 0x5200) { // ArtSync
+			sync_pending = true;
+			sync_received_time = millis();
+			return true;
+		}
+
+		if(op != 0x5000) // ArtDMX
+			return true;
+
+		uint8_t art_dmx_header[6];
+		if(udp.read(art_dmx_header, 6) != 6)
+			return true;
+
+		uint8_t seq = art_dmx_header[0];
+		uint16_t u = art_dmx_header[2] | (uint16_t(art_dmx_header[3]) << 8);
+		uint16_t size = art_dmx_header[5] | (uint16_t(art_dmx_header[4]) << 8);
 
 		if(u >= universes)
 			return true;
@@ -58,7 +92,20 @@ public:
 		if(size > 512)
 			return true;
 
+		if(seq != 0) {
+			if(last_seq[u] != 0) {
+				uint8_t lost = seq - last_seq[u] - 1;
+				if(seq <= last_seq[u]) lost--; // wrap around, seq skips 0
+				lost_frames[u] += lost;
+			}
+
+			last_seq[u] = seq;
+		}
+
+		unsigned read_start = micros();
 		int read = udp.read(data[u], size);
+		unsigned read_time = micros() - read_start;
+		read_max = read_time > read_max ? read_time : read_max;
 		assert(read == size);
 
 		return true;
