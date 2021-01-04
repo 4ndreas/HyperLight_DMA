5ec01527850de78fe4cad43e9334ded55b182a73
Sebastian Ottlik <git@sebastian-ottlik.de>
2021-01-04 14:35:33 +0100

    add spi hal code override

--- override_hal_spi.c
+++ override_hal_spi.c
@@ -1,16 +1,3 @@
- /******************************************************************************
-  * This file contains code from stm32f4xx_hal_spi.c by ST with additional modifications.
-  * Copyright (c) 2016 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-
 #include "stm32f4xx_hal.h"
 
 #define SPI_DEFAULT_TIMEOUT 100U
